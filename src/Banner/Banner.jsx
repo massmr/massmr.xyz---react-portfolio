7cce4b300e7ac839f6b0e61803c3cff96a8b3319
massmr <marcellin.massimo@gmail.com>
2024-02-05 08:51:27 +0100

    fix display in banner and modale

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,53 +1,19 @@
-import styled from 'styled-components'
-
-const BannerWrapper = styled.div`
-  display: flex;
-  flex-flow: column nowrap;
-  padding: 0 10px;
-  margin-top: 10px;
-  margin-bottom: 10px;
-`;
-
-const BannerTitle = styled.h1`
-  font-family: 'Major Mono Display', monospace;
-  font-size: 45px;
-  height: 60px;
-  color: #F8F9FA;
-`;
-
-const BannerSubTitle = styled.h2`
-   font-family: 'Major Mono Display', monospace;
-   font-size: 18px;
-   color: #F8F9FA;
-   margin: 15px 0 10px 0;
-`;
-
-const BannerText = styled.h4`
-  font-family: 'Jost', sans-serif;
-  font-size: 15px;
-  color: #F8F9FA;
-  font-weight: 300;
-`;
-
-const StrongText = styled.span`
-  font-weight: 900;
-  color: #9B5DE5;
-`; 
+import './Banner.css';
 
 export const Banner = () => {
   return (
     <>
-      <BannerWrapper>
+      <div className="banner-wrapper">
         
-        <BannerTitle>Massmr</BannerTitle>
+        <h1 className="banner-title">Massmr</h1>
         
-        <BannerSubTitle>web developer</BannerSubTitle>
-        
-        <BannerText>
-          Enter the <StrongText>digital realm</StrongText>
-        </BannerText>
+        <h2 className="banner-subtitle">web developer</h2>
+      
+        <h4 className="banner-text">
+          Enter the <span className="banner-text-strong">digital realm</span>
+        </h4>
 
-      </BannerWrapper>
+      </div>
     </>
   );
 };
