ec2c0168676bc57ad8a230be0b88a70845c485f7
massmr <marcellin.massimo@gmail.com>
2024-02-05 10:43:51 +0100

    setup display in desktop

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -4,15 +4,16 @@ export const Banner = () => {
   return (
     <>
       <div className="banner-wrapper">
-        
         <h1 className="banner-title">Massmr</h1>
-        
-        <h2 className="banner-subtitle">web developer</h2>
-      
-        <h4 className="banner-text">
-          Enter the <span className="banner-text-strong">digital realm</span>
-        </h4>
-
+        <div className="banner-text-wrapper">
+          <h2 className="banner-subtitle">web developer</h2>
+          <h4 className="banner-text">
+          Enter the 
+            <span className="banner-text-strong">
+              digital realm
+            </span>
+          </h4>
+        </div>
       </div>
     </>
   );
