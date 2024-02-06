181db50afb5f8daa40b60008c010807fe9df727b
massmr <marcellin.massimo@gmail.com>
2024-02-06 12:21:24 +0100

    fix grid height

--- src/GridOnFlex/Items/Item4.jsx
+++ src/GridOnFlex/Items/Item4.jsx
@@ -1,21 +1,10 @@
 import { useRef, useState } from 'react';
 
 import aboutMe720 from '../assets/aboutMe720.mp4';
-//import aboutMe440 from '../assets/aboutMe440.mp4';
 import aboutMeIMG from '../assets/aboutMeIMG.svg';
 import './Item4.css';
 
 export const Item4 = () => {
-  /*const windowWidth = useRef(window.innerWidth);
-  console.log(windowWidth);
-  
-  const selectSource = (width) => {
-    if(width >= 440) return aboutMe720;
-    return aboutMe440;
-  }
-  
-  const src = selectSource(windowWidth);*/
-
   const [isVideoLoaded, setIsVideoLoaded] = useState(false);
 
   const onLoadedData = () => {
