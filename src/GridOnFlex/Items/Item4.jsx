1201249ab63d7bee1b6532731677672413893f14
massmr <marcellin.massimo@gmail.com>
2024-02-06 11:47:15 +0100

    set video thumbnail

--- src/GridOnFlex/Items/Item4.jsx
+++ src/GridOnFlex/Items/Item4.jsx
@@ -1,8 +1,9 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 import aboutMe1080 from '../assets/aboutMe1080.mp4';
 import aboutMe720 from '../assets/aboutMe720.mp4';
 import aboutMe440 from '../assets/aboutMe440.mp4';
+import aboutMeIMG from '../assets/aboutMeIMG.svg';
 import './Item4.css';
 
 export const Item4 = () => {
@@ -17,10 +18,30 @@ export const Item4 = () => {
   
   const src = selectSource(windowWidth);
 
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
+  const onLoadedData = () => {
+    setIsVideoLoaded(true);
+  }
+
   return (
     <div className="item-4-wrapper">
-      <video playsInline autoPlay muted loop className="item-4-video">
+
+      <img src={aboutMeIMG}
+        alt="thumbnail"
+        className="item-4-thumbnail"
+        style={{ display: isVideoLoaded ? 'none' : 'block' }} />
+
+      <video playsInline 
+        autoPlay 
+        muted 
+        loop 
+        className="item-4-video"
+        onLoadedData={onLoadedData}
+        style={{ display : isVideoLoaded ? 'block' : 'none' }}>
+
         <source src={src} />
+
       </video>
     </div>
   );
