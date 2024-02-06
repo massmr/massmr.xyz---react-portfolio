9ccfb71dc0253115c57223a337bcc093e4c5c6e8
massmr <marcellin.massimo@gmail.com>
2024-02-06 16:40:51 +0100

    cancel education animation, use static image only

--- src/GridOnFlex/Items/Item7.jsx
+++ src/GridOnFlex/Items/Item7.jsx
@@ -1,5 +1,8 @@
-import education720 from '../assets/education-video.mp4'
+/*
+import education720 from '../assets/education-video.mp4';
 import educationIMG from '../assets/education-image.png';
+*/
+import education from '../assets/education-image-white.png';
 
 import './Item7.css';
 
@@ -10,13 +13,19 @@ export const Item7 = () => {
   return (
     <div className="item-7-wrapper">
 
-      <VideoItem  
-        thumbSource={educationIMG}
-        thumbAlt="education thumbnail"
-        thumbClass="item-7-thumbnail"
-        videoSource={education720}
-        videoClass="item-7-video"/>
+      <img src={education}
+        alt="education-image"
+        className="item-7-image" />
 
     </div>
   );
 };
+
+/*
+  <VideoItem  
+    thumbSource={educationIMG}
+    thumbAlt="education thumbnail"
+    thumbClass="item-7-thumbnail"
+    videoSource={education720}
+    videoClass="item-7-video"/>
+*/
