6634bb250d36dff8e8a0cc395e40b448ad25b532
massmr <marcellin.massimo@gmail.com>
2024-02-12 10:06:48 +0100

    refactor

--- src/LayoutsComponents/Projects/ProjectListItem.jsx
+++ src/LayoutsComponents/Projects/ProjectListItem.jsx
@@ -1,13 +1,3 @@
-export const ErrorProjectListItem = ({ project }) => {
-  const { name, description } = project;
-
-  return (
-    <div className="project-name">
-      <h1 className={`project-h1 ${isSelected ? 'project-h1-active' : '' }`}>{name}</h1>
-    </div>
-  ); 
-}; 
-
 export const ProjectListItem = ({ project }) => {
   const { name, description } = project;
 
