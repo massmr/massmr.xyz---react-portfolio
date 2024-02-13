6584c33175984e37f05340fe3980856b1aff2fa7
massmr <marcellin.massimo@gmail.com>
2024-02-13 12:35:11 +0100

    fix typo in project description

--- src/Utils/projects.js
+++ src/Utils/projects.js
@@ -1,7 +1,7 @@
 export const projects = [
   {
     name: "micheldupre.com",
-    description: "Michel Dupre, arenowned French fashion photographer and film director, collaborated with me to create his portfolio website, micheldupre.com. The site features curated galleries showcasing different styles, capturing Michel's diverse artistic talents. It also includes a news blog, offering insights into his latest projects and behind-the-scenes stories. This compact, multifaceted website provides a condensed yet immersive experience into Michel Dupre's creative world.",
+    description: "Michel Dupre, a renowned French fashion photographer and film director, collaborated with me to create his portfolio website, micheldupre.com. The site features curated galleries showcasing different styles, capturing Michel's diverse artistic talents. It also includes a news blog, offering insights into his latest projects and behind-the-scenes stories. This compact, multifaceted website provides a condensed yet immersive experience into Michel Dupre's creative world.",
     images: [
       {
         name: "image-1",
