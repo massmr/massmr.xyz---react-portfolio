2d6fdf08ced07c62d9cffc376faa281aa64c3e92
massmr <marcellin.massimo@gmail.com>
2024-02-16 15:10:43 +0100

    add Item2Page for mobile

--- src/LayoutsComponents/ItemPage/Item2Page.jsx
+++ src/LayoutsComponents/ItemPage/Item2Page.jsx
@@ -1,8 +1,144 @@
 import { ComingSoon } from '../ComingSoon.jsx'
+import { RegularList } from '../RegularList/RegularList.jsx'
+import { services } from '../../Utils/services.js'
+import  './Item2Page.css'
 
-export const Item2PageContent = ({ name }) => {
+export const Item2Preview = ({ name }) => {
 
   return (
     <ComingSoon />
+  );
+};
+
+export const Item2PageContent = ({ name }) => {
+  
+  return (
+    <>
+      <section className="service-container">
+        <RegularList
+          items={services}
+          resourceName="service"
+          ItemComponent={ServiceListItem} />
+      </section>
+    </>
+  );
+};
+
+//______________________________________________________________
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faPlus, faCheck } from "@fortawesome/free-solid-svg-icons"
+library.add({ faPlus, faCheck })
+import { useState, useEffect } from 'react'
+
+const ServiceListItem = ({ service }) => {
+  const { title, description, url, details } = service;
+
+  const [isShown, setIsShown] = useState(false);
+  const [animateDetails, setAnimateDetails] = useState(false);
+  
+  const handleShowDetails = () => {
+    setIsShown(true);
+  };
+  const handleHideDetails = () => {
+    setAnimateDetails(false);
+  }
+
+  //show useEffect
+  useEffect(() => {
+    if (isShown) {
+      const timeoutId = setTimeout(() => {
+        setAnimateDetails(true);
+      }, 50);
+      
+      return () => clearTimeout(timeoutId);
+    }   
+  }, [isShown]);
+  //hide useEffect
+  useEffect(() => {
+    if(!animateDetails) {
+      const timeoutId = setTimeout(() => {
+        setIsShown(false);
+      }, 600);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [animateDetails]);
+
+  return (
+    <div className="service-wrapper">
+      
+      <div className="service-title-wrapper">
+        <h2 className="service-title">{title}</h2>
+      </div>
+      
+      <div className="service-content-wrapper">
+        
+        <p className="service-description">{description}</p>
+        
+        <SeeDetailButton 
+          handleShowDetails={handleShowDetails} 
+          handleHideDetails={handleHideDetails} />
+        
+        { isShown && (
+          <div className={`service-detail-container ${animateDetails ? 'service-detail-container-active' : '' }`}>
+            <RegularList
+              items={details}
+              resourceName="detail"
+              ItemComponent={ServiceDetailListItem} />
+            <div className="go-to-contact-button-container">
+              <button className="go-to-contact-button">Ask for princing</button>
+            </div>
+          </div>
+        )}
+
+      </div>
+    </div>
+  );
+};
+
+//______________________________________________________________
+const SeeDetailButton = ({ 
+  handleShowDetails,
+  handleHideDetails,
+  }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const handleToggleHover = () => {
+    setIsHovered((prev) => !prev);
+  }
+  const handleToggleActivation = () => {
+    setIsActive((prev) => !prev);
+  }
+  const handleClick = () => {
+    handleShowDetails();
+    handleHideDetails();
+    handleToggleActivation();
+  }
+
+  return (
+    <div className="service-button-wrapper">
+      <p
+        onMouseEnter={handleToggleHover}
+        onMouseLeave={handleToggleHover}
+        onClick={handleClick}
+        className="service-button-text">See plan details</p>
+      <FontAwesomeIcon 
+        className={`service-button-icon ${(isHovered || isActive) ? 'service-button-icon-animated' : '' } ${isActive ? 'service-button-icon-active' : '' }`}
+        icon="fa-solid fa-plus" />
+    </div>
+  );
+};
+
+//______________________________________________________________
+const ServiceDetailListItem = ({ detail }) => {
+  //const { detail } = service;
+
+  return (
+    <div className="service-detail-wrapper">
+      <FontAwesomeIcon 
+        className="service-detail-icon"
+        icon="fa-solid fa-check" />
+      <p className="service-detail-text">{detail}</p>
+    </div>
   );
 };
---
+++ src/Utils/services.js
@@ -0,0 +1,70 @@
+export const services = [
+  {
+    title: 'Custom website plan',
+    description: "Elevate your business or personal presence with a website that reflects your needs. I will design and develop a professional and engaging site, leaving a lasting impression on clients or partners.",
+    url: 'svc1',
+    details: [
+      "Professionally styled, custom designs that match your branding and vision",
+      "Positive user experience navigation and designs",
+      "Responsive design",
+      "Lead-generating and performance-tested website copy",
+      "Designs and copy optimized for SEO",
+      "Ecommerce functionality",
+      "Database integration",
+      "New website page creation",
+      "Access to our in-house revenue acceleration software, MarketingCloudFX",
+      "Closed-loop call, lead, customer journey, and revenue tracking"
+    ]
+  },
+  {
+    title: 'Custom web app plan',
+    description: "Embark on a digital journey as I design and develop your personalized Web App to suit your vision. Experience your ideas coming to life, resulting in a sleek and functional online product.", 
+    url: 'svc2',
+    details: [
+      "Professionally styled, custom designs that match your branding and vision",
+      "Positive user experience navigation and designs",
+      "Responsive design",
+      "Lead-generating and performance-tested website copy",
+      "Designs and copy optimized for SEO",
+      "Ecommerce functionality",
+      "Database integration",
+      "New website page creation",
+      "Access to our in-house revenue acceleration software, MarketingCloudFX",
+      "Closed-loop call, lead, customer journey, and revenue tracking"
+    ]
+  },
+  {
+    title: 'Wodrpress features customization',
+    description: `Unleash the power of WordPress with my customization service. Personalize your website to match your brand perfectly. From themes to plugins, I'll ensure your WordPress site stands out in the crowd.`,
+    url: 'svc4',
+    details: [
+      "Professionally styled, custom designs that match your branding and vision",
+      "Positive user experience navigation and designs",
+      "Responsive design",
+      "Lead-generating and performance-tested website copy",
+      "Designs and copy optimized for SEO",
+      "Ecommerce functionality",
+      "Database integration",
+      "New website page creation",
+      "Access to our in-house revenue acceleration software, MarketingCloudFX",
+      "Closed-loop call, lead, customer journey, and revenue tracking"
+    ]
+  },
+  {
+    title: 'Wordpress debugging',
+    description: `Don't let bugs spoil your WordPress experience! I'm here to tackle any glitches and ensure your website runs smoothly. Say goodbye to pesky errors and hello to a seamless WordPress journey!`,
+    url: 'svc5',
+    details: [
+      "Professionally styled, custom designs that match your branding and vision",
+      "Positive user experience navigation and designs",
+      "Responsive design",
+      "Lead-generating and performance-tested website copy",
+      "Designs and copy optimized for SEO",
+      "Ecommerce functionality",
+      "Database integration",
+      "New website page creation",
+      "Access to our in-house revenue acceleration software, MarketingCloudFX",
+      "Closed-loop call, lead, customer journey, and revenue tracking"
+    ]
+  },
+]
