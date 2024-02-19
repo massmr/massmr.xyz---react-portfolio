9f2e11ca5694c4b064e0937caf975a091205c599
massmr <marcellin.massimo@gmail.com>
2024-02-19 16:28:06 +0100

    add of grid projects preview

--- src/GridOnFlex/Items/Item1.jsx
+++ src/GridOnFlex/Items/Item1.jsx
@@ -4,6 +4,7 @@ import './Item1.css';
 //import assets
 import { ImagesContainer } from '../ImagesContainer/ImagesContainer.jsx'
 import MDPortrait from "../../Utils/assets/img/FM20.jpg"
+import gridProject from "../../Utils/assets/img/grid-project.svg"
 
 export const Item1 = () => {
   
@@ -11,7 +12,7 @@ export const Item1 = () => {
     <>
       <ImagesContainer
         name="item1-image"
-        source={MDPortrait} 
+        source={gridProject} 
       />
       <button className="item-1-button">PROJECTS</button>
     </>
