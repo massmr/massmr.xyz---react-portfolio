79ea1aa8a9882052aedbef406e01a5f57c2a0e4d
massmr <marcellin.massimo@gmail.com>
2024-02-21 08:42:57 +0100

    useOnLoadMedia

---
+++ src/CustomHooks/checkOnLoad.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef, useState } from 'react'
+
+export const useOnLoadMedia = (ref) => {
+  const [status, setStatus] = useState(false);
+
+  useEffect(() => {
+    const updateStatus = (mediaElements) => {
+      setStatus(
+        mediaElements.map((media) => media.complete || media.readyState === 4).every((item) => item === true)
+      );
+    };
+
+    if (!ref.current) return;
+
+    const mediaLoaded = Array.from(ref.current.querySelectorAll('img, video'));
+
+    if (mediaLoaded.lenght === 0) {
+      setStatus(true);
+      return;
+    }
+
+    mediaLoaded.forEach((media) => {
+      media.addEventListener('load', () => updateStatus(mediaLoaded), {
+        once: true;
+      });
+      media.addEventListener('error', () => updateStatus(mediaLoaded), {
+        once: true;
+      });
+
+      if (media.tagName == 'VIDEO' && media.readyState === 4) {
+        updateStatus(mediaLoaded);
+      }
+    });
+
+    return () => {
+      mediaLoaded.forEach((media) => {
+        media.removeEventListener('load', () => updateStatus(mediaLoaded));
+        media.removeEventListener('error', () => updateStatus(mediaLoaded));
+      });    
+    };
+  }, [ref]);
+
+  return status;
+};
