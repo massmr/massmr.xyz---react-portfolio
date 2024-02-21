eceffd643a81b61e3a9939451688acbc18b0d666
massmr <marcellin.massimo@gmail.com>
2024-02-21 10:56:10 +0100

    hide some console.log

--- src/CustomHooks/checkOnLoad.jsx
+++ src/CustomHooks/checkOnLoad.jsx
@@ -4,9 +4,6 @@ export const useOnLoadMedia = (ref) => {
   const [status, setStatus] = useState(false);
 
   useEffect(() => {
-    console.log('Setting up event listeners...');
-    console.log('Ref:', ref);
-
     const updateStatus = (mediaElements) => {
       setStatus(
         mediaElements.map((media) => media.complete || media.readyState === 4).every((item) => item === true)
@@ -16,7 +13,6 @@ export const useOnLoadMedia = (ref) => {
     if (!ref.current) return;
 
     const mediaLoaded = Array.from(ref.current.querySelectorAll('img, video'));
-    console.log('Media elements:', mediaLoaded);
 
     if (mediaLoaded.length === 0) {
       setStatus(true);
@@ -37,9 +33,6 @@ export const useOnLoadMedia = (ref) => {
     });
 
     return () => {
-      console.log('Cleaning up event listeners...');
-      console.log('Ref:', ref);
-      console.log('Media elements:', mediaLoaded);
       mediaLoaded.forEach((media) => {
         media.removeEventListener('load', () => updateStatus(mediaLoaded));
         media.removeEventListener('error', () => updateStatus(mediaLoaded));
